2bbf604bcf9d1fdfd61a06ff69c217e2d26e6abb
BIEMAX <tecnico.beilke@gmail.com>
2022-04-01 09:54:04 -0300

    Added swagger docs

--- src/main/java/academy/springboot/controller/TemplateController.java
+++ src/main/java/academy/springboot/controller/TemplateController.java
@@ -1,18 +1,24 @@
 package academy.springboot.controller;
 
 import academy.springboot.domain.TemplateClass;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
+@Api(tags = "Template tag") //Swagger tag
 @RestController
-@RequestMapping("template")
+@RequestMapping("/v1/template") //Path of this controller
 public class TemplateController {
 
-    @GetMapping(path = "list") //localhost:8080/template/list
+    @ApiOperation("Get a list of names")
+    @GetMapping(path = "list") //subpath
+    @ResponseStatus(HttpStatus.OK)
     public List<TemplateClass> list() {
         return List.of(new TemplateClass("Julio"),
                         new TemplateClass("Roberta"),
--- src/main/java/academy/springboot/start/ApplicationStart.java
+++ src/main/java/academy/springboot/start/ApplicationStart.java
@@ -1,11 +1,11 @@
 package academy.springboot.start;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.springboot")
+@SpringBootApplication(scanBasePackages = "academy.springboot")
+@EnableSwagger2
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
