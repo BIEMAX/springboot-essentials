813e11ff4b187c6f381d3bce274dd1c2da532ff2
BIEMAX <tecnico.beilke@gmail.com>
2022-04-01 17:31:31 -0300

    Replace springfox swagger to spring-doc

--- src/main/java/academy/springboot/controller/TemplateController.java
+++ src/main/java/academy/springboot/controller/TemplateController.java
@@ -2,22 +2,21 @@ package academy.springboot.controller;
 
 import academy.springboot.domain.TemplateClass;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@Api(tags = "Template tag") //Swagger tag
+@RequiredArgsConstructor
 @RestController
+@Api(tags = "Template tag") //Swagger tag
 @RequestMapping("/v1/template") //Path of this controller
 public class TemplateController {
 
-    @ApiOperation("Get a list of names")
-    @GetMapping(path = "list") //subpath
+    @GetMapping(path = "list") //sub-path
     @ResponseStatus(HttpStatus.OK)
     public List<TemplateClass> list() {
         return List.of(new TemplateClass("Julio"),
