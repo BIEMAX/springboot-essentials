4231d517832edf3b84fe0fad3a2150e268418a0f
BIEMAX <tecnico.beilke@gmail.com>
2022-03-31 21:24:06 -0300

    Added lombok into TemplateClass

--- src/main/java/academy/springboot/domain/TemplateClass.java
+++ src/main/java/academy/springboot/domain/TemplateClass.java
@@ -1,4 +1,12 @@
 package academy.springboot.domain;
 
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class TemplateClass {
+    private String name;
 }
