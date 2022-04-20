ac980927957679c2d31236e2a03a5623aa3adb01
BIEMAX <tecnico.beilke@gmail.com>
2022-04-19 21:36:58 -0300

    Added console logging (for studie)

--- src/main/java/academy/springboot/start/ApplicationStart.java
+++ src/main/java/academy/springboot/start/ApplicationStart.java
@@ -2,10 +2,16 @@ package academy.springboot.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication(scanBasePackages = "academy.springboot")
 public class ApplicationStart {
+
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
+        //log.info("---------------------\nServer started at port: ", "8080");
     }
 }
