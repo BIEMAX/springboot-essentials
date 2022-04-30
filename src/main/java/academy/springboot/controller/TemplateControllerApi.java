a81f3d1c7ed6e58c4692c581248306b0c913812f
BIEMAX <tecnico.beilke@gmail.com>
2022-04-29 23:57:44 -0300

    Fixed issue #1

--- src/main/java/academy/springboot/controller/TemplateControllerApi.java
+++ src/main/java/academy/springboot/controller/TemplateControllerApi.java
@@ -2,27 +2,47 @@ package academy.springboot.controller;
 
 import academy.springboot.request.TemplateRequest;
 import academy.springboot.response.TemplateResponse;
+
+//Necessary for swagger ui documentation
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import lombok.RequiredArgsConstructor;
+import io.swagger.annotations.Example;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RestController
 @Api(tags = "Template tag") //Swagger tag
+@RestController
 @RequestMapping("/v1/template") //Path of this controller
 public interface TemplateControllerApi {
 
-    @ApiOperation(value = "Performs the registration of a new pix key for the client.")
-    @ApiResponses({
-            @ApiResponse(code = 201, message = "Key created successfully."),
-            @ApiResponse(code = 401, message = "Invalid access token."),
-            @ApiResponse(code = 404, message = "Customer not found."),
-            @ApiResponse(code = 503, message = "Unavailable service.")
+    @Operation(summary = "Template summary/intro", description = "Performs the registration of a new pix key for the client.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Key created successfully.",
+                    content = @Content(
+                            mediaType = "application/json", schema = @Schema(
+                            type = "SampleHttpResponseDto",
+                            example = "{\"status\":\"OK\",\"message\":\"sample OK answer\"}")
+                    )
+            ),
+            @ApiResponse(responseCode = "401", description = "Invalid access token."),
+            @ApiResponse(responseCode = "404", description = "Customer not found."),
+            @ApiResponse(responseCode = "503", description = "Unavailable service.")
     })
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
