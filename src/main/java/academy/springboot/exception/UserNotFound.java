f1b1c352dc25e76fbf1232741db76246cd499dfa
BIEMAX <tecnico.beilke@gmail.com>
2022-05-23 22:37:34 -0300

    Create new class exception

---
+++ src/main/java/academy/springboot/exception/UserNotFound.java
@@ -0,0 +1,11 @@
+package academy.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFound extends Exception {
+    public UserNotFound() {
+        super("User not found with this credentials");
+    }
+}
